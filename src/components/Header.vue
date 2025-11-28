d1624dcb88e39d74d92f24418ead973d3450286c
DeyvissonRobert <robert.designerx@gmail.com>
2025-11-28 19:48:00 -0300

    Iniciando Projeto

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+    <header>
+        <img src="../assets/logo.svg" alt="">
+
+        <nav>
+            <ul>
+                <li><a href="">
+                        <Icon icon="material-symbols:search-rounded" width="24" height="24" />
+                    </a></li>
+
+                <li><a href="">
+                        Início
+                        <Icon icon="material-symbols:home-outline-rounded" width="24" height="24" />
+                    </a></li>
+
+                <li><a href="">
+                        Galeria
+                        <Icon icon="material-symbols:home-storage-outline-sharp" width="24" height="24" />
+                    </a></li>
+
+                <li><a href="">
+                        Favoritos
+                        <Icon icon="material-symbols:favorite-outline" width="24" height="24" />
+                    </a></li>
+                    
+                <li><a href="">
+                        <Icon icon="material-symbols:person-outline" width="24" height="24" />
+                    </a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped lang="scss"></style>
