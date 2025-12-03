44e41c9a317bc4312895ba77083baced7c0e7a54
DeyvissonRobert <robert.designerx@gmail.com>
2025-12-03 19:41:12 -0300

    Inspire e Card criados

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import Header from './components/Header.vue';
 import Hero from './components/Hero.vue';
+import Inspire from './components/Inspire.vue';
 
 </script>
 
 <template>
   <Header />
   <Hero />
-
-<h2>InspiraPixel</h2>
+  <Inspire/>
 
 </template>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+defineProps(["imagem"]);
+</script>
+<template>
+    <div class="card">
+        <button>
+            <Icon icon="material-symbols:heart-plus-outline-rounded" width="24" height="24" />
+            <Icon icon="material-symbols:heart-plus-outline-rounded" width="24" height="24"  style="color: #fff" />
+            <Icon icon="material-symbols:heart-check-rounded" width="24" height="24" />
+            <Icon icon="material-symbols:heart-check-rounded" width="24" height="24"  style="color: #fff" />
+        </button>
+        <img :src="imagem"
+            alt="Praia de São Conrado" />
+    </div>
+</template>
+<style scoped lang="scss">
+.card {
+    width: 30vw;
+    height: 50vh;
+    position: relative;
+
+    img {
+        border-radius: 10px;
+        width: 100%;
+        height: 100%;
+    }
+
+    button {
+        position: absolute;
+        right: 10px;
+        background-color: transparent;
+        border: none;
+    }
+}
+</style>
