3cb5130ed9fc3bb4577de6aff88eb8b4ee5b6234
DeyvissonRobert <robert.designerx@gmail.com>
2025-12-08 20:03:49 -0300

    Iniciando Footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,44 @@
 import Header from './components/Header.vue';
 import Hero from './components/Hero.vue';
 import Inspire from './components/Inspire.vue';
+import Footer from './components/Footer.vue';
 
 </script>
 
 <template>
   <Header />
   <Hero />
-  <Inspire/>
+  <Inspire />
+  <Footer />
 
 </template>
 
 <style scoped lang="scss">
+/* Modo light */
+ $text: #040820;
+$background: #f2f3fd;
+$primary: #101e7a;
+$secondary: #ee77af;
+$accent: #b61619;
+
+/* Dark Mode */
+$text: #dfe3fb;
+$background: #02030d;
+$primary: #8593ef;
+$secondary: #881149;
+$accent: #e9494c;
+
+/* Modo light */
+$text: #0b0014;
+$background: #f8f0ff;
+$primary: #920bf9;
+$secondary: #fc7e82;
+$accent: #fba050;
+
+/* Dark Mode */
+$text: #f6ebff;
+$background: #08000f;
+$primary: #8d06f4;
+$secondary: #810308;
+$accent: #af5404;
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+
+</script>
+
+<template>
+    <footer>
+    <div class="redes">
+        <a href=""><Icon icon="ic:baseline-facebook" width="24" height="24"  style="color: #fff" /></a>
+        <a href=""><Icon icon="ri:linkedin-fill" width="24" height="24"  style="color: #fff" /></a>
+        <a href=""><Icon icon="mdi:instagram" width="24" height="24"  style="color: #fff" /></a>
+        <a href=""><Icon icon="mdi:twitter" width="24" height="24"  style="color: #fff" /></a>
+    
+    </div>
+
+    <div>
+        <h2>Contato</h2>
+        <a href="">contato@inspiralens.com</a>
+        <a href="">Central de ajuda</a>
+        <a href="">Rio de Janeiro – Brasil</a>
+    </div>
+    <div>
+        <h2>Sobre</h2>
+        <a href="">Politicas</a>
+        <a href="">Politicas de privacidade</a>
+        <a href="">Blog</a>
+    </div>
+
+    <div>
+        <h2>Assine nossa newsletter</h2>
+        <input type="email" placeholder="Seu email">
+        <button type="submit">Inscrever-se</button>
+    </div>
+
+</footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
